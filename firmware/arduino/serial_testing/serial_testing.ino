493205c9db1709dd84d60b44e954bd55cdd7d876
Jack O'Grady <jtogrady23@gmail.com>
2021-09-19 18:37:49 -0700

    completed code send and decode, integrating to main function

--- firmware/arduino/serial_testing/serial_testing.ino
+++ firmware/arduino/serial_testing/serial_testing.ino
@@ -35,57 +35,52 @@ int active_index;
 int active_state;
 bool send_data;
 
-// message processing
-const int CODE_ARRAY_SIZE = 3;
-int code_array[CODE_ARRAY_SIZE];
+// MESSAGE PROCESSING
+// --------------------
 
+// constants for message processing
+const char END_CHAR = '>';
+const char MESSAGE_TERMINATOR = '\n';
+
+// variables for holding the final processed codes
+const int CODE_ARRAY_SIZE = 5;
+int code_array[CODE_ARRAY_SIZE];
+int code_count;
+
+// interim variables for loading in straight serial data
 const int MAX_MESSAGE_CHARS = 32;
 char message_array[MAX_MESSAGE_CHARS];
 String serial_message;
 
-const char END_CHAR = '>';
-const char MESSAGE_TERMINATOR = '\n';
 
-// sends out the Arduino's status
-void broadcast_state(long start) {
-  Serial.print(STATUS_HEADER);
-  Serial.print(DELIMITER);
-  Serial.print(active_state);
-  Serial.print(DELIMITER);
-  Serial.print(active_listening);
-  Serial.print(DELIMITER);
-  Serial.println(millis() - start);
-}
+// FUNCTIONS
+// -------------------
 
-// sends out data from the Arduino
-void broadcast_data() {
-  // Data header
-  Serial.print(DATA_HEADER);
-  Serial.print(DELIMITER);
-  // motor 1 position,motor 2 position,solar power
-  Serial.print(servo_1_position);
-  Serial.print(DELIMITER);
-  Serial.print(servo_2_position);
-  // measure power
-  // FIXME -- add power signal
-}
-
+// broadcasts the processed codes that the Arduino just received
 void broadcast_code_array() {
-  Serial.print(code_array[0]);
-  Serial.print(DELIMITER);
-  Serial.print(code_array[1]);
-  Serial.print(DELIMITER);
-  Serial.println(code_array[2]);
+  // add protection for empty code
+  if (code_count != 0) {
+    // print all but last with delimiter
+    for (int i = 0; i < code_count - 1; i++) {
+      Serial.print(code_array[i]);
+      Serial.print(DELIMITER);
+    }
+    // print last w/ newline
+    Serial.println(code_array[code_count - 1]);
+  }
 }
 
-// populate code array from string input
+// processes input string to extract codes
 void process_input_message (String serial_message) {
   // find delimiters and endline
   int delim_index_array[10];
   int delim_count = 0;
-  int end_index;
-  
+  int end_index = 0;
+
+  // convert the string to an array of character
   serial_message.toCharArray(message_array, MAX_MESSAGE_CHARS);
+
+  // Identify the delimiter index locations and end character locations
   for (int i = 0; i < MAX_MESSAGE_CHARS; i++) {
     // find delimiter index
     if (message_array[i] == DELIMITER) {
@@ -97,12 +92,37 @@ void process_input_message (String serial_message) {
       end_index = i;
     }
   }
-  // validate that at least message end found
+
+  // If a valid input w/ end character, slice up array by delimiters to create an array of codes
   if (end_index != 0) {
-    // break up input into distinct values and populate code_array
+    int loop_start_index = 0;
+    int code_array_index = 0;
+    // code count keeps track of number of codes
+    code_count = 0;
+
+    // first get the values between delimiters
+    for (int i = 0; i < delim_count; i++) {
+      String temp_string;
+      for (int j = loop_start_index; j < delim_index_array[i]; j++) {
+        temp_string += message_array[j];
+      }
+      // add to array
+      code_array[code_array_index] = temp_string.toInt();
+      code_count++;
+      // increment store location in array
+      code_array_index += 1;
+      // increment start index
+      loop_start_index = delim_index_array[i] + 1;
+    }
     
+    // then add the last characters between final delim and end character
+    String temp_string;
+    for (int j = loop_start_index; j < end_index; j++) {
+      temp_string += message_array[j];
+    }
+    code_array[code_array_index] = temp_string.toInt();
+    code_count++;
   }
-  
 }
 
 void setup() {
@@ -112,7 +132,6 @@ void setup() {
   // Initialize Serial
   Serial.begin(SERIAL_BAUD_RATE);
  
-
   // tell Python that the program is initialized
   active_state = ACKNOWLEDGE;
   active_listening = 0;
@@ -128,18 +147,7 @@ void loop() {
     new_data = true;
   }
   if (new_data == true) {
-    serial_message.toCharArray(message_array, MAX_MESSAGE_CHARS);
-//    Serial.print(message_array[0]);
-//    Serial.print(DELIMITER);
-//    Serial.println(message_array[1]);
-    for (int i = 0; i < 20; i++) {
-      if (i != 19) {
-        Serial.print(message_array[i]);
-        Serial.print(DELIMITER);
-      }
-      else {
-        Serial.println(message_array[i]);
-      }
-    }
+    process_input_message(serial_message);
+    broadcast_code_array();
   }
 }
