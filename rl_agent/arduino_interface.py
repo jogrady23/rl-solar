960945d0d6a652843951d2cd8abfdd701f041deb
Jack O'Grady <jtogrady23@gmail.com>
2021-09-23 22:52:34 -0700

    updating python file with robust commands for arduino interface

--- rl_agent/arduino_interface.py
+++ rl_agent/arduino_interface.py
@@ -1,36 +1,84 @@
-import pandas as pd
-import time
 import serial
+import time
 
-# function to change motor to angle
+# Request codes
+MOTOR_CONTROL = 1000
+RESET_CODE = 6666
 
-# function to "move" panel by coordinating both motors
+# Response codes
+NOMINAL = 1111
+ERROR = 9999
 
-def readSerialLine(ser):
+# Constants for communication
+END_CHAR = '>'
+MESSAGE_TERMINATOR = '\n'
+DELIMITER = ','
+
+
+def read_serial_line(serial_device: serial.Serial):
     """
-    Reads data from Serial (from Arduino)
+       Reads data from Serial (from Arduino) with a safe check for end char
 
+       Args:
+           serial_device (serial.Serial): The Serial device
+       Returns:
+           (str): The output of the line, or None if invalid message
+    """
+    data_line = serial_device.readline().decode().strip()
+    if END_CHAR in data_line:
+        data_list = data_line.replace(END_CHAR, '').split(DELIMITER)
+    else:
+        data_list = None
+    return data_list
+
+
+def write_serial_line(serial_device: serial.Serial, code_array, write_timeout=3):
+    """
     Args:
-        ser(byte): Serial line from Arduino
-    Returns:
-        (str): The output of the line
-    """
-    line = ser.readline()
-    line = line.decode("utf-8")
-    data_line = line
-    line_output = data_line.strip()
-    return line_output
+        serial_device (serial.Serial): The Serial device
+        code_array (list): The sequence of codes/values to send to Arduino
 
+    Returns:
+        (bool): True for successful write, False for timeout
+
+    """
+    message = ','.join([str(x) for x in code_array]) + END_CHAR + MESSAGE_TERMINATOR
+
+    # Track write time or timeout
+    write_success = True
+    write_start = time.time()
+    serial_device.write(str(message).encode())
+
+    # Attempt to write message
+    print('Writing serial message...')
+    while (serial_device.out_waiting > 0) and (time.time() - write_start < write_timeout):
+        time.sleep(0.05)
+    if (time.time() - write_start > write_timeout) and (serial_device.out_waiting > 0):
+        write_success = False
+
+    # Reset buffer
+    serial_device.reset_output_buffer()
+
+    return write_success
+
+
+def initialize_serial(serial_port='/dev/cu.usbmodem14101', baud_rate=9600, timeout=2):
+    serial_device = serial.Serial(port=serial_port, baudrate=baud_rate, timeout=timeout)
+    serial_device.flush()
+    time.sleep(2)
+    return serial_device
+
+def loop():
+    # Testing
+    read_start = time.time()
+    while arduino.in_waiting <= 0 and time.time() - read_start < read_timeout:
+        time.sleep(0.05)
 
 if __name__ == '__main__':
     # Initialize serial port
-    serial_port = '/dev/cu.usbmodem14201'
+    serial_port = '/dev/cu.usbmodem14101'
     baud_rate = 9600
-    arduino = serial.Serial(port=serial_port, baudrate=baud_rate, timeout=1)
-    time.sleep(2)
+    timeout = 3
+    arduino = initialize_serial(serial_port=serial_port, baud_rate=baud_rate, timeout=timeout)
 
-    # Tell the Arduino to take the initial OCV measurement
-    while arduino.in_waiting() < 0:
-        time.sleep(0.1)
-    arduinoData = readSerialLine(arduino)
-    initialVoltage = float(arduinoData) / 2 # divide by 2 since SoC(OCV) function is for 1 cell's OCV
+    #
